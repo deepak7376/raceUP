12c68880b2ea12f20b908c8d5bea994380ae4d42
Deepak Yadav <dky.united@gmail.com>
2020-01-05 15:32:26 +0530

    pre-alpha

---
+++ setup.py
@@ -0,0 +1,42 @@
+
+from setuptools import setup, find_packages
+  
+# reading long description from file 
+with open('DESCRIPTION.txt') as file: 
+    long_description = file.read() 
+  
+  
+# specify requirements of your package here 
+REQUIREMENTS = ['requirements.txt'] 
+  
+# some more details 
+CLASSIFIERS = [ 
+    'Development Status :: 2 - Pre-Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Internet', 
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.3', 
+    'Programming Language :: Python :: 3.4', 
+    'Programming Language :: Python :: 3.5', 
+    ] 
+  
+# calling the setup function  
+setup(name='raceUP', 
+      version='1.0.0', 
+      description='2D car racing game based on pygame', 
+      long_description="interesting game", 
+      url='https://github.com/deepak7376/raceUP', 
+      author='Deepak Yadav', 
+      author_email='dky.united@gmail.com', 
+      license='MIT', 
+      packages=find_packages(), 
+      classifiers=CLASSIFIERS, 
+      install_requires=REQUIREMENTS, 
+      keywords='car race pygame',
+      include_package_data=True,
+      zip_safe=False
+
+      ) 
+
+
---
+++ src/play.py
@@ -0,0 +1,212 @@
+import pygame
+import time
+import random
+
+pygame.init()
+#crash_sound = pygame.mixer.Sound('Glass_Crunch.mp3')
+
+# initialization
+display_width=800
+display_height=600
+
+gameDisplay=pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("RaceUP")
+clock=pygame.time.Clock()
+
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+green=(0,255,0)
+brightGreen=(0,200,0)
+brightRed=(200,0,0)
+
+pause=False
+img=pygame.image.load("images/racecar.png")
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text,width,height,size):
+    largeText = pygame.font.Font('freesansbold.ttf',size)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((width,height))
+    gameDisplay.blit(TextSurf, TextRect)
+     
+def crashed():
+##    pygame.mixer.music.load('Crash.mp3')
+##    pygame.mixer.music.play(-1)
+    
+    message_display("Game Over!",display_width/2,display_height/2,70)
+    #pygame.mixer.Sound.play(crash_sound)
+    pygame.mixer.music.stop()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitGame()
+        button("PlayAgain",150,450,100,50,green,brightGreen,gameLoop)
+        button("Quit",550,450,100,50,red,brightRed,quitGame)
+        
+        pygame.display.update()
+        clock.tick(15)   
+
+        
+
+    
+def thing(x,y,h,w):
+    pygame.draw.rect(gameDisplay,red,[x,y,w,h])
+
+def button(msg,x,y,w,h,ia,ac,action=None):
+    mouse=pygame.mouse.get_pos()
+    click=pygame.mouse.get_pressed()
+    
+
+    if x<mouse[0]<x+w and y<mouse[1]<y+h:
+        pygame.draw.rect(gameDisplay,ac,(x,y,w,h))
+        if click[0]==1 and action!=None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay,ia,(x,y,w,h))
+
+    message_display(msg,x+(w/2),y+(h/2),20)
+##    largeText = pygame.font.Font('freesansbold.ttf',20)
+##    TextSurf, TextRect = text_objects(msg, largeText)
+##    TextRect.center = ( (x+(w/2)), (y+(h/2)) )
+##    gameDisplay.blit(TextSurf, TextRect)
+##   
+def quitGame():
+    pygame.quit()
+    quit()
+
+def score(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("score: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
+
+def unpaused():
+    pygame.mixer.music.unpause()
+    global pause
+    pause=False
+def paused():
+    ############
+    pygame.mixer.music.pause()
+    #############
+    
+    message_display("Paused",display_width/2,display_height/2,100)
+##    largeText = pygame.font.SysFont("comicsansms",115)
+##    TextSurf, TextRect = text_objects("Paused", largeText)
+##    TextRect.center = ((display_width/2),(display_height/2))
+##    gameDisplay.blit(TextSurf, TextRect)
+##    
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitGame()
+
+        button("Continue",150,450,100,50,green,brightGreen,unpaused)
+        button("Quit",550,450,100,50,red,brightRed,quitGame)
+
+        pygame.display.update()
+        clock.tick(15)   
+
+def gameIntro():
+    # pygame.mixer.music.load('sounds/Rocking_Chair.mp3')
+    # pygame.mixer.music.play(-1)
+    intro=True
+    
+    while intro:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                quitGame()
+        gameDisplay.fill(white)
+        
+        message_display("RaceUP",display_width/2,display_height/2,115)
+        message_display("Devloped by Deepak",700,550,10)
+        
+##        largeText = pygame.font.Font('freesansbold.ttf',115)
+##        TextSurf, TextRect = text_objects("RaceUP", largeText)
+##        TextRect.center = ((display_width/2),(display_height/2))
+##        gameDisplay.blit(TextSurf, TextRect)
+##
+##        largeText = pygame.font.Font('freesansbold.ttf',115)
+##        TextSurf, TextRect = text_objects("RaceUP", largeText)
+##        TextRect.center = ((display_width/2),(display_height/2))
+##        gameDisplay.blit(TextSurf, TextRect)
+    
+        
+        button("GO!",150,450,100,50,green,brightGreen,gameLoop)
+        button("Quit",550,450,100,50,red,brightRed,quitGame)
+        
+        pygame.display.update()
+        clock.tick(15)
+    
+def gameLoop():
+    # pygame.mixer.music.load('sounds/Race_Car.mp3')
+    # pygame.mixer.music.play(-1)
+    global pause
+    imgPos_x=(display_width * 0.45)
+    imgPos_y=(display_height * 0.8)
+    carWidth=73
+    x=0
+    count=0
+    thingX=200
+    thingY=50
+    thingH=80
+    thingW=80
+    thingS=15
+
+    gameExit=False
+
+    while not gameExit:
+        
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                quitGame()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    x=-10
+                if event.key==pygame.K_RIGHT:
+                    x=10
+                if event.key==pygame.K_p:
+                    pause=True
+                    paused()
+                    
+            if event.type==pygame.KEYUP:
+                if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+                    x=0
+
+        
+
+        imgPos_x+=x
+        
+
+        if imgPos_x>display_width-carWidth or imgPos_x<0:
+            crashed()
+
+        if imgPos_y<thingY+thingH:
+            if thingX<imgPos_x<thingX+thingW or thingX<imgPos_x+carWidth<thingX+thingW :
+                crashed()
+
+        if thingY>display_height:
+            thingY=-thingH
+            count+=1
+            thingX=random.randrange(0,display_width-thingW)
+            thingW+=5
+
+        gameDisplay.fill(white)
+        thing(thingX,thingY,thingH,thingW)
+        thingY+=thingS
+        score(count)
+
+        gameDisplay.blit(img,(imgPos_x,imgPos_y))
+
+        
+        pygame.display.update()
+        clock.tick(30)
+
+if __name__=='__main__':
+    gameIntro()
+    pygame.quit()
+    quit()
